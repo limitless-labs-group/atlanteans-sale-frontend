fe543e7daca8aef94545bb856cfc1841db69ae15
Max Schnaider <maxschnaider@pm.me>
2023-03-07 20:06:19 +0200

    Types: token

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,5 +1,16 @@
 import { providers, Signer } from 'ethers'
+import { Address } from 'wagmi'
+
+export { Signer }
 
 export type SignerOrProvider = Signer | providers.BaseProvider
 
 export type MerkleProof = string[]
+
+export interface Token {
+  address: Address
+  symbol: string
+  decimals: number
+  name?: string
+  logoURI?: string
+}
