13488a5091dd67fe832c838fe0ce5968507a0027
Max Schnaider <maxschnaider@pm.me>
2023-03-07 20:08:06 +0200

    Sale contract: abi update, testnet address

--- src/contracts/constants/addresses.ts
+++ src/contracts/constants/addresses.ts
@@ -1,12 +1,12 @@
-import { MAINNET_CHAIN, TESTNET_CHAIN } from '@/constants'
+import { MAINNET_CHAIN, SupportedChainId, TESTNET_CHAIN } from '@/constants'
 import { Address } from 'wagmi'
 
-export const ATLANTEANS_CONTRACT_ADDRESS: { [chainId: number]: Address } = {
-  [TESTNET_CHAIN.id]: '0x',
-  [MAINNET_CHAIN.id]: '0x',
+export const ATLANTEANS_CONTRACT_ADDRESS: { [chainId in SupportedChainId]: Address } = {
+  [TESTNET_CHAIN?.id]: '0x4B028F5E138D2FDFB8A5F2a5a1fe643749Fbb3Df',
+  [MAINNET_CHAIN?.id]: '0x',
 }
 
-export const ATLANTEANS_SALE_CONTRACT_ADDRESS: { [chainId: number]: Address } = {
-  [TESTNET_CHAIN.id]: '0x',
-  [MAINNET_CHAIN.id]: '0x',
+export const ATLANTEANS_SALE_CONTRACT_ADDRESS: { [chainId in SupportedChainId]: Address } = {
+  [TESTNET_CHAIN?.id]: '0xe61347A7649D262b2Bcf60F10c956711D50C23Bb',
+  [MAINNET_CHAIN?.id]: '0x',
 }
