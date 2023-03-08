e4421300d51f6eed3ad06e7cc3a3d1b9d4ae83e7
Max Schnaider <maxschnaider@pm.me>
2023-03-08 20:17:52 +0200

    types: chain, address, re-export libs types

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,11 +1,13 @@
-import { providers, Signer } from 'ethers'
-import { Address } from 'wagmi'
+import { providers, Signer, Bytes, BigNumber, ContractTransaction } from 'ethers'
 
-export { Signer }
+export { Signer, BigNumber }
+export type { Bytes, ContractTransaction }
 
 export type SignerOrProvider = Signer | providers.BaseProvider
 
 export type MerkleProof = string[]
+
+export type Address = `0x${string}`
 
 export interface Token {
   address: Address
@@ -13,4 +15,43 @@ export interface Token {
   decimals: number
   name?: string
   logoURI?: string
+}
+
+export interface Chain {
+  id: number
+  network: string
+  name: string
+  nativeCurrency: {
+    name: string
+    symbol: string
+    decimals: number
+  }
+  rpcUrls: {
+    alchemy?: {
+      http: string[]
+      webSocket?: string[]
+    }
+    infura?: {
+      http: string[]
+      webSocket?: string[]
+    }
+    default: {
+      http: string[]
+      webSocket?: string[]
+    }
+    public: {
+      http: string[]
+      webSocket?: string[]
+    }
+  }
+  blockExplorers: {
+    etherscan?: {
+      name: string
+      url: string
+    }
+    default: {
+      name: string
+      url: string
+    }
+  }
 }
